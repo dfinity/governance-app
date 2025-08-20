c412e00187bdb9e0f8bd8515dac5b1fd01ea2bbd
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-08-20 17:19:09 +0200

    Home labels

--- src/governance-app-frontend/src/pages/homepage/Homepage.tsx
+++ src/governance-app-frontend/src/pages/homepage/Homepage.tsx
@@ -1,4 +1,5 @@
 import { useInternetIdentity } from 'ic-use-internet-identity';
+import { useTranslation } from 'react-i18next';
 
 import { useTheme } from '@/common/hooks/useTheme';
 import { useQueryUpdateCall } from '@/common/queries/useQueryUpdateCall';
@@ -6,6 +7,7 @@ import { useQueryUpdateCall } from '@/common/queries/useQueryUpdateCall';
 function Homepage() {
   const { login, identity } = useInternetIdentity();
   const { theme, toggleTheme } = useTheme();
+  const { t, i18n } = useTranslation(undefined, { keyPrefix: 'demo' });
 
   const data = useQueryUpdateCall<{
     message: string;
@@ -31,14 +33,14 @@ function Homepage() {
   return (
     <main className="p-4">
       <div>
-        <h1 className="text-4xl font-bold">The Governance App</h1>
+        <h1 className="text-4xl font-bold">{t('headline')}</h1>
         <img src="/logo2.svg" alt="DFINITY logo" className="py-4" />
         <button
           onClick={login}
           disabled={identity !== undefined}
           className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
         >
-          Login with Internet Identity!
+          {t('login')}
         </button>
       </div>
 
@@ -51,21 +53,26 @@ function Homepage() {
           </div>
 
           <div className="text-2xl pt-4">
-            Hello world! You are:{' '}
-            <span className="underline italic text-purple-500">
-              {identity?.getPrincipal().toString()}
-            </span>
+            {t('yourPrincipal', { principal: identity?.getPrincipal().toString() ?? '' })}
           </div>
         </>
       )}
 
       <div className="text-xs pt-4 flex items-center gap-2">
-        You are using the theme: {theme}
+        {t('selectedTheme', { theme })}
         <button
           className="rounded bg-blue-500 px-2 py-1 text-white hover:bg-blue-600 uppercase font-bold"
           onClick={toggleTheme}
         >
-          Toggle theme
+          {t('toggleTheme')}
+        </button>
+      </div>
+      <div className="text-xs pt-4 flex items-center gap-2">
+        <button
+          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'de' : 'en')}
+        >
+          {t('changeLang', { lang: i18n.language === 'en' ? 'Deutsch' : 'English' })}
         </button>
       </div>
     </main>
