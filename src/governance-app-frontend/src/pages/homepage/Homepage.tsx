b4e659cda3d6f84dd3e0e1fbd51957049c8df7c1
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-08-21 13:19:17 +0200

    Fix labels

--- src/governance-app-frontend/src/pages/homepage/Homepage.tsx
+++ src/governance-app-frontend/src/pages/homepage/Homepage.tsx
@@ -7,7 +7,7 @@ import { useQueryUpdateCall } from '@/common/queries/useQueryUpdateCall';
 function Homepage() {
   const { login, identity } = useInternetIdentity();
   const { theme, toggleTheme } = useTheme();
-  const { t, i18n } = useTranslation(['home', 'common']);
+  const { t, i18n } = useTranslation(['common', 'home']);
 
   const data = useQueryUpdateCall<{
     message: string;
@@ -33,38 +33,38 @@ function Homepage() {
   return (
     <main className="p-4">
       <div>
-        <h1 className="text-4xl font-bold">{t('headline')}</h1>
+        <h1 className="text-4xl font-bold">{t('home:headline')}</h1>
         <img src="/logo2.svg" alt="DFINITY logo" className="py-4" />
         <button
           onClick={login}
           disabled={identity !== undefined}
           className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
         >
-          {t('common:loading')}
+          {t('common:login')}
         </button>
       </div>
 
       {identity !== undefined && (
         <>
           <div className="pt-4">
-            {data.isLoading && <p>Loading...</p>}
+            {data.isLoading && <p>{t('common:loading')}</p>}
             {data.isError && <p>Error: {data.error.message}</p>}
             {data.data && <p>{data.data.data.message}</p>}
           </div>
 
           <div className="text-2xl pt-4">
-            {t('yourPrincipal', { principal: identity?.getPrincipal().toString() ?? '' })}
+            {t('home:yourPrincipal', { principal: identity?.getPrincipal().toString() ?? '' })}
           </div>
         </>
       )}
 
       <div className="text-xs pt-4 flex items-center gap-2">
-        {t('selectedTheme', { theme })}
+        {t('home:selectedTheme', { theme })}
         <button
           className="rounded bg-blue-500 px-2 py-1 text-white hover:bg-blue-600 uppercase font-bold"
           onClick={toggleTheme}
         >
-          {t('toggleTheme')}
+          {t('home:toggleTheme')}
         </button>
       </div>
       <div className="text-xs pt-4 flex items-center gap-2">
@@ -72,7 +72,7 @@ function Homepage() {
           className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
           onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'de' : 'en')}
         >
-          {t('changeLang', { lang: i18n.language === 'en' ? 'Deutsch' : 'English' })}
+          {t('home:changeLang', { lang: i18n.language === 'en' ? 'Deutsch' : 'English' })}
         </button>
       </div>
     </main>
