1f1dc52ae336300b07ca7ea6660de815d33248da
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-08-28 13:56:24 +0200

    add useGovernanceGetProposal

---
+++ src/governance-app-frontend/src/common/hooks/canisters/governance/useGovernanceGetProposal.ts
@@ -0,0 +1,47 @@
+import { ListProposalsRequest } from '@dfinity/nns';
+
+import { useQueryThenUpdateCall } from '@queries/useQueryThenUpdateCall';
+import { QUERY_KEYS } from '@utils/queryKeys';
+
+import { useNnsGovernanceCanister } from './useGovernanceCanister';
+
+/**
+ * Use the `list_proposals` API instead of `proposal_info` to take advantage of `omitLargeFields`.
+ * The payload rendering will rely on new backend fields (not yet implemented).
+ */
+export const useGovernanceGetProposal = ({ proposalId }: { proposalId: bigint | undefined }) => {
+  const { ready, canister } = useNnsGovernanceCanister();
+  console.log(proposalId && proposalId + 1n);
+
+  const request: ListProposalsRequest = {
+    beforeProposal: proposalId && proposalId + 1n,
+    limit: 1,
+    excludeTopic: [],
+    includeRewardStatus: [],
+    includeStatus: [],
+    includeAllManageNeuronProposals: false,
+    omitLargeFields: true,
+  };
+
+  return useQueryThenUpdateCall({
+    queryKey: [
+      QUERY_KEYS.NNS_GOVERNANCE.LIST_PROPOSALS,
+      {
+        ...request,
+        // TODO: Replace with Francesco’s version
+        beforeProposal: request.beforeProposal ? request.beforeProposal.toString() : undefined,
+      },
+    ],
+    queryFn: async () => {
+      const res = await canister!.listProposals({ request, certified: false });
+      return res.proposals[0];
+    },
+    updateFn: async () => {
+      const res = await canister!.listProposals({ request, certified: true });
+      return res.proposals[0];
+    },
+    options: {
+      enabled: ready && !!proposalId,
+    },
+  });
+};
