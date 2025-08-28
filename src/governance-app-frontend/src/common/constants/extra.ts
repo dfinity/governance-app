66f7b9f024491f7aa933c02645cbd983e8fff2f8
Francesco <francesco.rizzi@dfinity.org>
2025-08-28 14:57:40 +0200

    real pagination

--- src/governance-app-frontend/src/common/constants/extra.ts
+++ src/governance-app-frontend/src/common/constants/extra.ts
@@ -3,5 +3,5 @@ export const HOST = process.env.DFX_HOST || 'localhost:8080';
 export const LOCAL_NETWORK = `http://${HOST}`;
 export const MAINNET_NETWORK = 'https://ic0.app';
 
-export const PAGINATION_LIMIT = 1;
+export const PAGINATION_LIMIT = 100;
 export const MIN_ASYNC_DELAY = 200; // Avoids flashing of loading indicators.
