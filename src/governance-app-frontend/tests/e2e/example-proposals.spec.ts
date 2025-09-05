a300c83249aef06dcdee017a7512af340584243e
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-05 11:59:36 +0200

    wip

---
+++ src/governance-app-frontend/tests/e2e/example-proposals.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  page.on('console', (msg) => {
+    console.log(`[browser] ${msg.type()}: ${msg.text()}`);
+  });
+
+  await page.goto('http://localhost:3000/nns/proposals');
+  await expect(page.getByTestId('main-layout')).toBeVisible();
+
+  await expect(page.getByTestId('proposals')).toBeVisible();
+  await expect(page.getByTestId('proposals').locator('a')).toHaveCount(6);
+});
