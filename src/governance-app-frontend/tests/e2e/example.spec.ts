df30b66dabc93cf44c249bef9a03761d273ed8b6
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-05 09:26:15 +0200

    wip

--- src/governance-app-frontend/tests/e2e/example.spec.ts
+++ src/governance-app-frontend/tests/e2e/example.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { login } from './utils/login';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/The Governance App/);
+
+  await login({ page });
+
+  await expect(page.getByTestId('login-test')).toHaveText(/Hello world! You are: /);
 });
---
+++ src/governance-app-frontend/tests/e2e/utils/login.ts
@@ -0,0 +1,27 @@
+import { expect, test, type BrowserContext, type Page } from '@playwright/test';
+
+export const login = async ({ page }: { page: Page }) => {
+  await test.step('Login', async (step) => {
+    const [newTab] = await Promise.all([
+      page.waitForEvent('popup'), // catches the new tab
+      page.getByTestId('login-btn').click(),
+    ]);
+
+    await page.getByTestId('login-btn').click();
+
+    await expect(newTab).toHaveTitle('Internet Identity');
+
+    await newTab.waitForLoadState('networkidle');
+
+    await newTab.getByRole('button', { name: 'Create Internet Identity' }).click();
+    await newTab.getByRole('button', { name: 'Create Passkey' }).click();
+
+    // create new identity
+    await newTab.locator('input#captchaInput').fill('a');
+    await newTab.getByRole('button', { name: 'Next' }).click();
+    await newTab.getByRole('button', { name: 'Continue' }).click();
+
+    await newTab.waitForEvent('close'); // wait until user is redirected back and tab closes
+    await expect(newTab.isClosed()).toBe(true);
+  });
+};
