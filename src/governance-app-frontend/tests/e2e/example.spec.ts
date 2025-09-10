53172177c38c61345591b40f44b65c17d9fe0750
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-10 10:35:19 +0200

    baseUrl

--- src/governance-app-frontend/tests/e2e/example.spec.ts
+++ src/governance-app-frontend/tests/e2e/example.spec.ts
@@ -1,40 +1,23 @@
 import { test, expect } from '@playwright/test';
 import { login } from './utils/login';
-import { takeSnapshot } from './utils/take-snapshot';
 
 test('has title', async ({ page }) => {
   page.on('console', (msg) => {
     console.log(`[browser] ${msg.type()}: ${msg.text()}`);
   });
-  console.log('✅ Page loaded');
 
-  // page.on('request', (req) => {
-  //   console.log('🚀HEADERS:');
-  //   console.log(req.method());
-  //   console.log(req.headers());
-  //   console.log(req.url());
-  // });
-
-  // TODO: get the url from env?
-  // await page.goto('http://localhost:3000/');
-  await page.goto('http://lqy7q-dh777-77777-aaaaq-cai.localhost:8080');
-  console.log('✅ Page0', await page.title());
+  // const url = 'http://localhost:3000/';
+  // const url = 'http://lqy7q-dh777-77777-aaaaq-cai.localhost:8080';
+  await page.goto('/');
 
   await page.waitForLoadState('networkidle'); // ensures all assets loaded
   await expect(page.getByTestId('main-layout')).toBeVisible({ timeout: 15000 });
+  await expect(page).toHaveTitle('The Governance App');
 
-  console.log('✅ Page1', await page.title());
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/The Governance App/);
   // await takeSnapshot({ page, label: 'home-signed-out' });
 
-  console.log('✅ Page2');
   await login({ page });
-
-  console.log('✅ Page3');
   await expect(page.getByTestId('login-test')).toHaveText(/Hello world! You are: /);
-  console.log('✅ Page4');
 
   // await takeSnapshot({ page, label: 'home-signed-in' });
 });
--- src/governance-app-frontend/tests/e2e/utils/login.ts
+++ src/governance-app-frontend/tests/e2e/utils/login.ts
@@ -1,32 +1,18 @@
-import { expect, Locator, test, type Page } from '@playwright/test';
-import { log } from 'console';
-
-// pick whichever is visible first
-const firstVisibleLocatorIndex = (locators: Locator[]): Promise<number> =>
-  Promise.race(
-    locators.map((locator, index) =>
-      locator.waitFor({ state: 'visible', timeout: 5000 }).then(() => index),
-    ),
-  ).catch(() => -1);
+import { expect, test, type Page } from '@playwright/test';
+import { firstVisibleLocatorIndex } from './e2e';
 
 export const login = async ({ page }: { page: Page }) => {
   await test.step('Login', async () => {
-    console.log('✅ Login 0');
     await expect(page.getByTestId('login-btn')).toBeVisible();
     await expect(page.getByTestId('login-btn')).toBeEnabled();
 
-    console.log('✅ Login 1');
     const [newTab] = await Promise.all([
       page.waitForEvent('popup'), // catches the new tab
       page.getByRole('button', { name: 'Login' }).click(),
     ]);
-    console.log('✅ Login 2');
 
     await newTab.waitForLoadState('networkidle'); // ensures all assets loaded
-
     await expect(newTab).toHaveTitle(/Internet Identity/);
-    console.log('✅ new title', await newTab.title());
-    console.log(await newTab.innerHTML('body'));
 
     // create new identity
     await newTab.getByRole('button', { name: 'Create Internet Identity' }).click();
@@ -36,19 +22,16 @@ export const login = async ({ page }: { page: Page }) => {
     const visibleIndex = await firstVisibleLocatorIndex([passkeyBtn, continueBtn]);
 
     if (visibleIndex === 1) {
+      console.log('🧪 ii: I saved it, continue', await newTab.title());
       await continueBtn.click();
     } else {
+      console.log('🧪 ii: Create Passkey', await newTab.title());
       await passkeyBtn.click();
       await newTab.getByRole('button', { name: 'Create Passkey' }).click();
       await newTab.locator('input#captchaInput').fill('a');
       await newTab.getByRole('button', { name: 'Next' }).click();
       await newTab.getByRole('button', { name: 'Continue' }).click();
     }
-
-    // if (await newTab.getByRole('button', { name: 'I saved it, continue' }).isVisible()) {
-    //   await newTab.getByRole('button', { name: 'I saved it, continue' }).click();
-    // } else {
-    // }
 
     await newTab.waitForEvent('close'); // wait until user is redirected back and tab closes
     await expect(newTab.isClosed()).toBe(true);
