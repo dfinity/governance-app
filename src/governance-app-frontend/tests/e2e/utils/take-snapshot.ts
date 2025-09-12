0794aa5fa3b33f021c3f61fe1e8f959890efd3a2
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-12 10:48:13 +0200

    no fullpage

--- src/governance-app-frontend/tests/e2e/utils/take-snapshot.ts
+++ src/governance-app-frontend/tests/e2e/utils/take-snapshot.ts
@@ -5,13 +5,13 @@ export const takeSnapshot = async ({ page, label }: { page: Page; label: string 
     // iPhone 16 pro viewport
     await page.setViewportSize({ width: 402, height: 874 });
     await expect(page).toHaveScreenshot(`${label}-402x874.png`, {
-      fullPage: true,
+      // fullPage: true,
     });
 
     // MacBook Air 13.3 viewport
     await page.setViewportSize({ width: 1440, height: 900 });
     await expect(page).toHaveScreenshot(`${label}-1440x900.png`, {
-      fullPage: true,
+      // fullPage: true,
     });
   });
 };
