d91a9ab064bfb7aa8ad733fc84ed235b096b1278
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-12 16:22:18 +0200

    formatting

--- src/governance-app-frontend/tests/e2e/login.spec.ts
+++ src/governance-app-frontend/tests/e2e/login.spec.ts
@@ -1,4 +1,5 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
+
 import { login } from './utils/login';
 import { takeSnapshot } from './utils/take-snapshot';
 
