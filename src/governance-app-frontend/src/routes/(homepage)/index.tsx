2c3eefba6e111954c68239e44e424d2c918b4d33
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-18 16:03:27 +0200

    wip

--- src/governance-app-frontend/src/routes/(homepage)/index.tsx
+++ src/governance-app-frontend/src/routes/(homepage)/index.tsx
@@ -11,7 +11,7 @@ function Homepage() {
   const { t } = useTranslation();
 
   return (
-    <div className="text-md" data-testid="login-test" data-snapshot-mask>
+    <div className="text-lg" data-testid="login-test" data-snapshot-mask>
       {identity
         ? t(($) => $.home.yourPrincipal, {
             principal: identity.getPrincipal().toString(),
