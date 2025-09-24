b88db9a0ac959c3f01557d860251376e83a10a80
Francesco <francesco.rizzi@dfinity.org>
2025-09-24 13:32:03 +0200

    wip

---
+++ src/governance-app-frontend/src/common/hooks/canisters/icpIndex/useIcpIndexTransactions.ts
@@ -0,0 +1,47 @@
+import { AnonymousIdentity } from '@dfinity/agent';
+import { AccountIdentifier, GetAccountIdentifierTransactionsResponse } from '@dfinity/ledger-icp';
+import { Option } from '@dfinity/nns';
+import { useInternetIdentity } from 'ic-use-internet-identity';
+
+import { PAGINATION_LIMIT } from '@constants/extra';
+import { useInfiniteQueryThenUpdateCall } from '@queries/useInfiniteQueryThenUpdateCall';
+import { QUERY_KEYS } from '@utils/queryKeys';
+
+import { useIcpIndex } from './useIcpIndex';
+
+export const useIcpIndexTransactions = () => {
+  const { identity } = useInternetIdentity();
+  const { ready, authenticated, canister } = useIcpIndex();
+
+  // If no identity is present, we use an anonymous identity to avoid errors.
+  // The query will be disabled anyway if not authenticated.
+  const accountIdentifier = AccountIdentifier.fromPrincipal({
+    principal: identity?.getPrincipal() || new AnonymousIdentity().getPrincipal(),
+  });
+
+  return useInfiniteQueryThenUpdateCall<GetAccountIdentifierTransactionsResponse, Option<bigint>>({
+    queryKey: [QUERY_KEYS.ICP_INDEX.TRANSACTIONS],
+    queryFn: (context) =>
+      canister!.getTransactions({
+        maxResults: BigInt(PAGINATION_LIMIT),
+        start: context.pageParam,
+        accountIdentifier,
+        certified: false,
+      }),
+    updateFn: (context) =>
+      canister!.getTransactions({
+        maxResults: BigInt(PAGINATION_LIMIT),
+        start: context.pageParam,
+        accountIdentifier,
+        certified: true,
+      }),
+    initialPageParam: 0n,
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.response.transactions.length === PAGINATION_LIMIT
+        ? BigInt(allPages.length * PAGINATION_LIMIT)
+        : undefined,
+    options: {
+      enabled: ready && authenticated,
+    },
+  });
+};
--- src/governance-app-frontend/src/common/utils/queryKeys.ts
+++ src/governance-app-frontend/src/common/utils/queryKeys.ts
@@ -4,13 +4,18 @@ const ICP_LEDGER = {
   ACCOUNT_BALANCE: 'icpLedgerAccountBalance',
   METADATA: 'icpLedgerMetadata',
 };
+
 const NNS_GOVERNANCE = {
   PROPOSALS: 'nnsGovernanceProposals',
   PROPOSAL: 'nnsGovernanceProposal',
   NEURONS: 'nnsGovernanceNeurons',
 };
 
-export const QUERY_KEYS = { ICP_LEDGER, NNS_GOVERNANCE };
+const ICP_INDEX = {
+  TRANSACTIONS: 'icpIndexTransactions',
+};
+
+export const QUERY_KEYS = { ICP_LEDGER, NNS_GOVERNANCE, ICP_INDEX };
 
 export const stringifyKeys = (keys: readonly unknown[]) => {
   return keys.map((k) => {
