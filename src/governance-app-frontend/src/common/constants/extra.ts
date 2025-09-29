2b9bba3483d565bb2d3aa98392e74a82f1347219
Max Strasinsky <maksims.strasinskis@dfinity.org>
2025-09-29 11:57:52 +0200

    dissolve delay consts

--- src/governance-app-frontend/src/common/constants/extra.ts
+++ src/governance-app-frontend/src/common/constants/extra.ts
@@ -11,3 +11,7 @@ export const E8Sn = 100_000_000n;
 export const ICP_TRANSACTION_FEE_E8S = 10_000n;
 
 export const VOTING_RESULTS_PRECISION = 6; // Number of digits after the decimal point.
+
+export const SECONDS_IN_DAY = 60 * 60 * 24;
+// Taking into account 1/4 of leap year
+export const SECONDS_IN_YEAR = ((4 * 365 + 1) * SECONDS_IN_DAY) / 4;
