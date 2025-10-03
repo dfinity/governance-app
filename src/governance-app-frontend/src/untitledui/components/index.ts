1e53b5f56f5a17115e4c906b3bdc5ac9a372c0e9
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-10-03 10:23:41 +0200

    centralize exports

--- src/governance-app-frontend/src/untitledui/components/index.ts
+++ src/governance-app-frontend/src/untitledui/components/index.ts
@@ -1,7 +1,15 @@
 export { Avatar } from './base/avatar/avatar';
 export { Badge, BadgeWithDot, BadgeWithIcon } from './base/badges/badges';
+export { Input } from './base/input/input';
 export { Link } from './base/link/link';
 export { Tag, TagGroup, TagList } from './base/tags/tags';
 export { Tooltip } from './base/tooltip/tooltip';
+
+export {
+  Dialog,
+  DialogTrigger,
+  Modal,
+  ModalOverlay
+} from './application/modals/modal';
 
 export { Button } from './custom/buttons/button';
