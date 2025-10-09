8ba9ac8f639752ea6ae3ba1536bf5751c6b8a4ff
Francesco <francesco.rizzi@dfinity.org>
2025-10-09 17:41:26 +0200

    wip

--- src/governance-app-frontend/src/routes/(homepage)/index.tsx
+++ src/governance-app-frontend/src/routes/(homepage)/index.tsx
@@ -2,6 +2,11 @@ import { createFileRoute } from '@tanstack/react-router';
 import { useInternetIdentity } from 'ic-use-internet-identity';
 import { useTranslation } from 'react-i18next';
 
+import { QueryStates } from '@components/extra/QueryStates';
+import { SkeletonLoader } from '@components/loaders/SkeletonLoader';
+import { CANISTER_ID_ICP_LEDGER } from '@constants/canisterIds';
+import { TokenPrices, useIcpSwapPrices } from '@hooks/externalServices/useIcpSwapPrices';
+
 export const Route = createFileRoute('/(homepage)/')({
   component: Homepage,
 });
@@ -10,6 +15,8 @@ function Homepage() {
   const { identity } = useInternetIdentity();
   const { t } = useTranslation();
 
+  const prices = useIcpSwapPrices();
+
   return (
     <div className="text-xl" data-testid="login-test" data-snapshot-mask>
       {identity
@@ -17,6 +24,18 @@ function Homepage() {
             principal: identity.getPrincipal().toString(),
           })
         : t(($) => $.common.loginWithII)}
+
+      <QueryStates<TokenPrices>
+        query={prices}
+        isEmpty={(data) => !Object.values(data).length}
+        loadingComponent={<SkeletonLoader width={100} height={24} />}
+      >
+        {(data) => (
+          <div>
+            {t(($) => $.common.ICPtoUSD)}: {data[CANISTER_ID_ICP_LEDGER!]?.usd}$
+          </div>
+        )}
+      </QueryStates>
     </div>
   );
 }
