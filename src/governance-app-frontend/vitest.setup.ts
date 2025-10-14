bf23056c9207a343a507cf53115b5b7bf348e142
Francesco <francesco.rizzi@dfinity.org>
2025-10-14 10:32:22 +0200

    WIP - Work in progress!

--- src/governance-app-frontend/vitest.setup.ts
+++ src/governance-app-frontend/vitest.setup.ts
@@ -2,6 +2,7 @@ import { vi } from 'vitest';
 
 // Mock environment variables
 vi.stubEnv('CANISTER_ID_CKUSD_LEDGER', 'xevnm-gaaaa-aaaar-qafnq-cai');
+vi.stubEnv('CANISTER_ID_ICP_LEDGER', 'ryjl3-tyaaa-aaaaa-aaaba-cai');
 
 // Mock matchMedia
 Object.defineProperty(globalThis, 'matchMedia', {
