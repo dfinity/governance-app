f850b8cb2650697d94a38f3bb0251faee71dd7cf
Francesco <francesco.rizzi@dfinity.org>
2025-10-15 16:24:35 +0200

    WIP - Work in progress!

---
+++ src/governance-app-frontend/tests/e2e/stubs/IcpSwap.ts
@@ -0,0 +1,14 @@
+import { Page } from '@playwright/test';
+
+import { ICP_SWAP_URL } from '@constants/externalServices';
+import { fixture_IcpSwapTickers_Alice, fixture_IcpSwapTickers_ckUSDC } from '@fixtures/IcpSwap';
+
+export const stubIcpSwap = async (page: Page) => {
+  await page.route(`${ICP_SWAP_URL}/tickers`, async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify([...fixture_IcpSwapTickers_Alice, fixture_IcpSwapTickers_ckUSDC]),
+    });
+  });
+};
--- src/governance-app-frontend/tests/e2e/utils/app.ts
+++ src/governance-app-frontend/tests/e2e/utils/app.ts
@@ -1,17 +1,10 @@
 import { expect, type Page } from '@playwright/test';
 
-import { ICP_SWAP_URL } from '@constants/externalServices';
-import { fixture_IcpSwapTickers_Alice, fixture_IcpSwapTickers_ckUSDC } from '@fixtures/IcpSwap';
+import { stubIcpSwap } from '../stubs/IcpSwap';
 
 export const openApp = async ({ page, url = '/' }: { page: Page; url?: string }) => {
-  // Stub ICP Swap API with test data for consistent screenshots.
-  await page.route(`${ICP_SWAP_URL}/tickers`, async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      body: JSON.stringify([...fixture_IcpSwapTickers_Alice, fixture_IcpSwapTickers_ckUSDC]),
-    });
-  });
+  // Stub external services with test data for consistent behavior.
+  await stubIcpSwap(page);
 
   await page.goto(url);
   await page.waitForLoadState('networkidle'); // ensures all assets loaded
