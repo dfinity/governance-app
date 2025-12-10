d472d3e713545be53d423ac00d638fbb8632b50c
Yusef Habib <yusef.fernandez@dfinity.org>
2025-12-10 15:42:15 +0100

    fix(auth): handle UserInterrupt during login as unauthenticated state (#65)
    # Motivation
    
    When a user closes the II page during the login flow,
    [useAuth](https://github.com/dfinity/icp-js-core/blob/651c6565d0e065b94ab99215f5c0b69a88d34354/packages/auth-client/src/index.ts#L497)
    throws a `UserInterrupt` error. Currently, this error is not handled,
    causing the app to fail.
    
    
    https://github.com/user-attachments/assets/8aefc805-3b83-46cc-a3a0-f4f5751d1ad5
    
    # Changes
    
    - Handle `UserInterrupt` and treat it the same as a non-authorized user.

--- src/governance-app-frontend/src/common/utils/router.ts
+++ src/governance-app-frontend/src/common/utils/router.ts
@@ -1,3 +1,4 @@
+import { ERROR_USER_INTERRUPT } from '@icp-sdk/auth/client';
 import { redirect } from '@tanstack/react-router';
 import { ensureInitialized } from 'ic-use-internet-identity';
 
@@ -6,7 +7,14 @@ import i18n from '@/i18n/config';
 import { warningNotification } from './notification';
 
 export const requireIdentity = async () => {
-  const identity = await ensureInitialized();
+  let identity;
+
+  try {
+    identity = await ensureInitialized();
+  } catch (err) {
+    // If user interrupts the login flow by closing the InternetIdentiy page, we just swallow the error (the user is just not authenticated, it is not an error in our app)
+    if (!(err instanceof Error && err.message === ERROR_USER_INTERRUPT)) throw err;
+  }
 
   if (!identity) {
     console.log('[🔐 Protected Route]: identity not found, redirecting to homepage.');
