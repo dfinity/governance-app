09f548ca93762896b1af888b08061bafaf117ca9
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-12-11 14:54:46 +0100

    login

--- src/governance-app-frontend/src/routes/(homepage)/index.tsx
+++ src/governance-app-frontend/src/routes/(homepage)/index.tsx
@@ -1,15 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useInternetIdentity } from 'ic-use-internet-identity';
+import { useTranslation } from 'react-i18next';
 
 import { AccountCard } from '@features/account/components/AccountCard';
+
+import { Button } from '@components/button';
 
 export const Route = createFileRoute('/(homepage)/')({
   component: Homepage,
 });
 
 function Homepage() {
+  const { t } = useTranslation();
+  const { identity, login } = useInternetIdentity();
+
   return (
     <div className="flex flex-col gap-4">
-      <AccountCard />
+      {identity ? (
+        <AccountCard />
+      ) : (
+        <Button onClick={login} variant="outline" size="lg">
+          {t(($) => $.common.login)}
+        </Button>
+      )}
     </div>
   );
 }
