abe89c460d6ea4a63974fb022247f183bda70457
Francesco <francesco.rizzi@dfinity.org>
2025-12-11 11:47:42 +0100

    WIP - Work in progress!

--- src/governance-app-frontend/src/common/utils/staking-rewards.spec.ts
+++ src/governance-app-frontend/src/common/utils/staking-rewards.spec.ts
@@ -4,6 +4,7 @@ import {
   type NeuronInfo,
   NeuronState,
 } from '@icp-sdk/canisters/nns';
+import { describe, it } from 'vitest';
 
 import { E8S, SECONDS_IN_EIGHT_YEARS } from '@constants/extra';
 
@@ -61,4 +62,10 @@ export const getApyTestNeuron = (
     },
     autoStakeMaturity: true,
   },
+});
+
+describe('staking-rewards utils', () => {
+  it('TODO: should add tests for staking-rewards utility functions', () => {
+    // TODO: implement unit tests
+  });
 });
