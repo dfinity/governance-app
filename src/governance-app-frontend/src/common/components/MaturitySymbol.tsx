737d62f140476cfbdd3203056f1ba89dc734f2e2
Francesco <francesco.rizzi@dfinity.org>
2025-12-19 13:36:01 +0100

    WIP - Work in progress!

--- src/governance-app-frontend/src/common/components/MaturitySymbol.tsx
+++ src/governance-app-frontend/src/common/components/MaturitySymbol.tsx
@@ -8,7 +8,7 @@ interface MaturitySymbolProps {
 export function MaturitySymbol({ onClick, className = '' }: MaturitySymbolProps) {
   return (
     <div
-      className={`cursor-pointer rounded-sm border border-amber-400 bg-amber-100 p-0.5 transition-all duration-300 hover:scale-110 ${className} `}
+      className={`cursor-help rounded-sm border border-amber-400 bg-amber-100 p-0.5 transition-all duration-300 hover:scale-110 ${className} `}
       onClick={onClick}
       role="button"
     >
