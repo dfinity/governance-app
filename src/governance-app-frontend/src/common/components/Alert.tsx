f61c53ed96741b4a4990e2a532a7857e47251c54
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-12-23 15:28:03 +0100

    rebase

--- src/governance-app-frontend/src/common/components/Alert.tsx
+++ src/governance-app-frontend/src/common/components/Alert.tsx
@@ -4,7 +4,6 @@ import * as React from 'react';
 import { cn } from '@common/utils/shadcn';
 
 const alertVariants = cva(
-  'relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current',
   'relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current text-muted-foreground',
   {
     variants: {
@@ -16,8 +15,6 @@ const alertVariants = cva(
           'border-green-200 bg-green-50 text-green-800 [&>svg]:text-green-600 *:data-[slot=alert-description]:text-green-700',
         warning:
           'bg-amber-50 border-amber-200 text-amber-800 dark:bg-amber-900/20 dark:border-amber-900/50 dark:text-amber-200 [&>svg]:text-current',
-        destructive:
-          'text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90',
       },
     },
     defaultVariants: {
