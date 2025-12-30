5f6d68d6296dff6422d7b7eb96467f2e1572ac15
Yusef Habib Fernandez <habibfernandez@gmail.com>
2025-12-30 09:34:53 +0100

    fmt

--- src/governance-app-frontend/src/features/onboarding/WelcomeModal.tsx
+++ src/governance-app-frontend/src/features/onboarding/WelcomeModal.tsx
@@ -2,6 +2,9 @@ import { Vote } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { Button } from '@components/button';
+import { WELCOME_MODAL_STORAGE_KEY } from '@constants/extra';
+import { successNotification } from '@utils/notification';
 import {
   ResponsiveDialog,
   ResponsiveDialogContent,
@@ -9,9 +12,6 @@ import {
   ResponsiveDialogFooter,
   ResponsiveDialogTitle,
 } from '@common/components/ResponsiveDialog';
-import { Button } from '@components/button';
-import { WELCOME_MODAL_STORAGE_KEY } from '@constants/extra';
-import { successNotification } from '@utils/notification';
 
 export function WelcomeModal() {
   const { t } = useTranslation();
