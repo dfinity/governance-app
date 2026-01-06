4ecf82a96b36da28fc26694c6f190a503131256a
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-01-06 17:11:17 +0100

    quick fix send icp

--- src/governance-app-frontend/src/common/constants/extra.ts
+++ src/governance-app-frontend/src/common/constants/extra.ts
@@ -15,7 +15,6 @@ export const E8Sn = BigInt(E8S);
 export const U64_MAX = 2n ** 64n - 1n;
 
 export const ICP_MIN_STAKE_AMOUNT = 1;
-export const ICP_MIN_TRANSFER_AMOUNT = 1;
 export const ICP_TRANSACTION_FEE = 0.0001;
 export const ICP_TRANSACTION_FEE_E8S = ICP_TRANSACTION_FEE * E8S;
 export const ICP_TRANSACTION_FEE_E8Sn = BigInt(ICP_TRANSACTION_FEE_E8S);
