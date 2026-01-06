3662a1793e817a89cca036799a115c3d9ce8fef5
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-01-06 12:25:43 +0100

    remove skeleton for the whole layout

--- src/governance-app-frontend/src/common/components/MainLayout.tsx
+++ src/governance-app-frontend/src/common/components/MainLayout.tsx
@@ -1,4 +1,3 @@
-import { useInternetIdentity } from 'ic-use-internet-identity';
 import { ReactNode } from 'react';
 
 import { WelcomeModal } from '@features/onboarding/WelcomeModal';
@@ -6,34 +5,19 @@ import { WelcomeModal } from '@features/onboarding/WelcomeModal';
 import { BottomNav } from '@components/navigation/BottomNav';
 import { Header } from '@components/navigation/Header';
 import { Sidebar } from '@components/navigation/Sidebar';
-import { SkeletonLoader } from '@components/SkeletonLoader';
-import { useAgentPool } from '@hooks/useAgentPool';
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
-  const { isInitializing } = useInternetIdentity();
-
-  const { anonymous, authenticated } = useAgentPool().agentPool;
-  const showLoader = anonymous.loading || authenticated.loading || isInitializing;
-
   return (
     <div className="flex min-h-screen w-full bg-background" data-testid="main-layout">
-      {showLoader ? (
-        <div className="h-full w-full p-4">
-          <SkeletonLoader count={6} />
-        </div>
-      ) : (
-        <>
-          <WelcomeModal />
-          <Sidebar />
-          <div className="flex h-screen w-full flex-col overflow-hidden">
-            <Header />
-            <main className="relative mb-20 flex-1 overflow-auto p-6 lg:mb-0">
-              <div className="mx-auto w-full max-w-5xl">{children}</div>
-            </main>
-            <BottomNav />
-          </div>
-        </>
-      )}
+      <WelcomeModal />
+      <Sidebar />
+      <div className="flex h-screen w-full flex-col overflow-hidden">
+        <Header />
+        <main className="relative mb-20 flex-1 overflow-auto p-6 lg:mb-0">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </main>
+        <BottomNav />
+      </div>
     </div>
   );
 };
