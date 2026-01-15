f7e3b36bd6a997ea57480b519b2cd165b89be12a
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-01-15 15:10:52 +0100

    [15/01 15:29]: [15/01 15:10]: clean up

--- src/governance-app-backend/src/tvl/state.rs
+++ src/governance-app-backend/src/tvl/state.rs
@@ -6,17 +6,3 @@ pub struct TvlState {
     pub total_locked_icp_e8s: u64,
     pub last_update_timestamp_seconds: u64,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    impl TvlState {
-        pub fn test_data() -> Self {
-            Self {
-                total_locked_icp_e8s: 12_345_678_900_000_000,
-                last_update_timestamp_seconds: 1_234_567_890,
-            }
-        }
-    }
-}
