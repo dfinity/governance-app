1dfe0d73df307b30d407f2f819d5b19b0a6cbb1a
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-01-19 11:45:32 +0100

    [19/01 23:08]: [19/01 11:45]: simplify sorting

--- src/governance-app-frontend/src/features/voting/utils/knownNeurons.ts
+++ src/governance-app-frontend/src/features/voting/utils/knownNeurons.ts
@@ -36,11 +36,5 @@ export const sortKnownNeurons = (a: KnownNeuron, b: KnownNeuron) => {
   if (a.id === DFINITY_NEURON_ID) return -1;
   if (b.id === DFINITY_NEURON_ID) return 1;
 
-  const aHasTopics = (a.committed_topics?.length ?? 0) > 0;
-  const bHasTopics = (b.committed_topics?.length ?? 0) > 0;
-
-  if (aHasTopics && !bHasTopics) return -1;
-  if (!aHasTopics && bHasTopics) return 1;
-
   return a.id < b.id ? -1 : 1;
 };
