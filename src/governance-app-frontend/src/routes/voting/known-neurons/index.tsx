9d35a6fe0320f98b5acd11130416f284eea5be5e
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-01-21 11:49:38 +0100

    [21/01 11:49]: import

--- src/governance-app-frontend/src/routes/voting/known-neurons/index.tsx
+++ src/governance-app-frontend/src/routes/voting/known-neurons/index.tsx
@@ -19,6 +19,7 @@ import { useGovernanceKnownNeurons } from '@hooks/governance/useGovernanceKnownN
 import useTitle from '@hooks/useTitle';
 import { warningNotification } from '@utils/notification';
 import { QUERY_KEYS } from '@utils/query';
+import { requireIdentity } from '@utils/router';
 import {
   AlertDialog,
   AlertDialogAction,
