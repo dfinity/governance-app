01d84638686e08b68fb7eae0e9d885a1114a8307
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-01-23 19:27:45 +0100

    [23/01 19:27]: merge

--- src/governance-app-frontend/src/routes/_auth.tsx
+++ src/governance-app-frontend/src/routes/_auth.tsx
@@ -2,9 +2,11 @@ import { createFileRoute, Outlet } from '@tanstack/react-router';
 
 import { BetaBanner } from '@components/BetaBanner';
 import { MainLayout } from '@components/MainLayout';
+import { analytics } from '@features/analytics/service';
 import { useSessionCountdownToast } from '@hooks/useSessionCountdownToast';
 import { useThemeShortcut } from '@hooks/useThemeShortcut';
 import { requireIdentity } from '@utils/router';
+import { useEffect } from 'react';
 
 export const Route = createFileRoute('/_auth')({
   beforeLoad: requireIdentity,
@@ -14,6 +16,10 @@ export const Route = createFileRoute('/_auth')({
 function AuthLayout() {
   useThemeShortcut();
   useSessionCountdownToast();
+
+  useEffect(() => {
+    analytics.init();
+  }, []);
 
   return (
     <MainLayout>
