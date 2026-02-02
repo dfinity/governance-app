6acd4414bab4fe17def6d4acfd512feb78fba8b5
Francesco <francesco.rizzi@dfinity.org>
2026-02-02 11:38:49 +0100

    [02/02 11:38 - yhabib/clean-up]: remove unsused translations

--- src/governance-app-frontend/src/common/components/PageHeader.tsx
+++ src/governance-app-frontend/src/common/components/PageHeader.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react';
 
-type PageHeaderProps = {
+type Props = {
   title: string;
   description?: string;
   actions?: ReactNode;
 };
 
-export const PageHeader = ({ title, description, actions }: PageHeaderProps) => {
+export const PageHeader = ({ title, description, actions }: Props) => {
   return (
     <header className="flex flex-col gap-6 sm:flex-row sm:justify-between">
       <div className="flex flex-col gap-2">
