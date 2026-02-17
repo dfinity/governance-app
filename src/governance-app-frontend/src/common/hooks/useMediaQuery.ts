0c907aa16a7901ed4dee91705520e5109cf83f24
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-02-17 12:05:36 +0100

    [17/02 15:24]: [17/02 14:02]: [17/02 12:05]: remove ssr as it doesn't bring anything for our use case

--- src/governance-app-frontend/src/common/hooks/useMediaQuery.ts
+++ src/governance-app-frontend/src/common/hooks/useMediaQuery.ts
@@ -10,7 +10,6 @@ export function useMediaQuery(query: string) {
     [query],
   );
   const getSnapshot = useCallback(() => matchMedia(query).matches, [query]);
-  const getServerSnapshot = useCallback(() => false, []);
 
-  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+  return useSyncExternalStore(subscribe, getSnapshot);
 }
