b1661636c2b3196d05ab37a1ea434789d857965d
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-02-20 13:04:16 +0100

    [20/02 13:04]: remove no longer used hook

--- src/governance-app-frontend/src/common/hooks/governance/index.ts
+++ src/governance-app-frontend/src/common/hooks/governance/index.ts
@@ -1,6 +1,5 @@
 export { useNnsGovernance } from './useGovernance';
 export { useGovernanceEconomics } from './useGovernanceEconomics';
-export { useGovernanceLatestRewardEvent } from './useGovernanceLatestRewardEvent';
 export { useGovernanceMetrics } from './useGovernanceMetrics';
 export { useGovernanceNeurons } from './useGovernanceNeurons';
 export { useGovernanceProposal } from './useGovernanceProposal';
