caefe1ea0523e9b0197c2297bdd1614b4d7f3789
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-03-06 19:28:28 +0100

    [06/03 19:28]: fmt

--- src/governance-app-frontend/src/features/voting/components/KnownNeuronCard.tsx
+++ src/governance-app-frontend/src/features/voting/components/KnownNeuronCard.tsx
@@ -1,5 +1,5 @@
-import { nonNullish } from '@dfinity/utils';
 import type { KnownNeuron } from '@icp-sdk/canisters/nns';
+import { nonNullish } from '@dfinity/utils';
 import { ChevronDown, ChevronUp, Circle, CircleDot, LinkIcon, Loader2 } from 'lucide-react';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
