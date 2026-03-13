95aba0a1c10e862154d5d18f2b2ba92af45ce7d0
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-03-13 12:50:02 +0100

    [13/03 17:11]: [13/03 12:50]: [13/03 12:50]: usememo

--- src/governance-app-frontend/src/features/accounts/hooks/useMainAccountMetadata.ts
+++ src/governance-app-frontend/src/features/accounts/hooks/useMainAccountMetadata.ts
@@ -1,6 +1,5 @@
 import { AccountIdentifier } from '@icp-sdk/canisters/ledger/icp';
 import { useInternetIdentity } from 'ic-use-internet-identity';
-import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { AccountType, type AccountMetadata } from '../types';
@@ -9,17 +8,15 @@ export const useMainAccountMetadata = () => {
   const { t } = useTranslation();
   const { identity } = useInternetIdentity();
 
-  const data: AccountMetadata | undefined = useMemo(() => {
-    if (!identity) return undefined;
-
-    return {
-      name: t(($) => $.accounts.mainAccount),
-      accountId: AccountIdentifier.fromPrincipal({
-        principal: identity.getPrincipal(),
-      }).toHex(),
-      type: AccountType.Main,
-    };
-  }, [identity, t]);
+  const data: AccountMetadata | undefined = identity
+    ? {
+        name: t(($) => $.accounts.mainAccount),
+        accountId: AccountIdentifier.fromPrincipal({
+          principal: identity.getPrincipal(),
+        }).toHex(),
+        type: AccountType.Main,
+      }
+    : undefined;
 
   return { data, isLoading: false };
 };
