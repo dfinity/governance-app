817579b70ba1bd04d7a0dfe694aa62a22e258f0f
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-03-24 11:40:10 +0100

    [24/03 15:48]: [24/03 11:40]: clean up rebase

--- src/governance-app-frontend/src/features/stakes/components/NeuronsList.tsx
+++ src/governance-app-frontend/src/features/stakes/components/NeuronsList.tsx
@@ -100,32 +100,6 @@ export const NeuronsList = ({
         isOpen={isStandaloneModalOpen && standaloneAction === NeuronStandaloneAction.StakeMaturity}
         onOpenChange={handleModalClose}
       />
-
-      {selectedNeuron && (
-        <>
-          <DisburseIcpModal
-            neuron={selectedNeuron}
-            isOpen={
-              isStandaloneModalOpen && standaloneAction === NeuronStandaloneAction.DisburseIcp
-            }
-            onOpenChange={handleModalClose}
-          />
-          <DisburseMaturityModal
-            neuron={selectedNeuron}
-            isOpen={
-              isStandaloneModalOpen && standaloneAction === NeuronStandaloneAction.DisburseMaturity
-            }
-            onOpenChange={handleModalClose}
-          />
-          <StakeMaturityModal
-            neuron={selectedNeuron}
-            isOpen={
-              isStandaloneModalOpen && standaloneAction === NeuronStandaloneAction.StakeMaturity
-            }
-            onOpenChange={handleModalClose}
-          />
-        </>
-      )}
     </div>
   );
 };
