d643b5403e10fce225f9fab764832f52b19abd2a
Yusef Habib Fernandez <habibfernandez@gmail.com>
2026-04-10 14:55:04 +0200

    [10/04 14:55]: [10/04 14:55]: car

--- src/governance-app-frontend/src/common/hooks/tickers/useTickerPrices.ts
+++ src/governance-app-frontend/src/common/hooks/tickers/useTickerPrices.ts
@@ -7,7 +7,7 @@ export enum TickerPricesSource {
 }
 
 export const useTickerPrices = () => {
-  const exchangeRate = useExchangeRate({ enabled: true, retryCount: 3 });
+  const exchangeRate = useExchangeRate({ enabled: true, retryCount: 1 });
   const useFallback = exchangeRate.isError;
   const icpSwapPrices = useIcpSwapPrices({ enabled: useFallback, retryCount: 1 });
 
